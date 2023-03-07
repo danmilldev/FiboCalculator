3b1402e27f3f287bc697e9ef77b3568f7b731086
danmilldev <silbercrisis63@gmail.com>
2023-03-07 17:06:56 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("How much Fibo Numbers do you wish?");
+Console.Write("Amount: ");
+
+int amount = Convert.ToInt32(Console.ReadLine());
+
+List<int> fiboList = new List<int> { 0, 1, 1 };
+
+for (int i = 2; i < amount; i++)
+{
+    fiboList.Add(fiboList[i] + fiboList[i - 1]);
+}
+
+Console.WriteLine("Fibo Numbers:");
+
+foreach (var fibo in fiboList)
+{
+    Console.WriteLine(fibo);
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# FiboCalculator
+ 
